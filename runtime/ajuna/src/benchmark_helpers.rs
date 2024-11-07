f00ae2be88ac2873ce5ae3c2fbcc18d6e7e3de9b
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-11-07 10:38:55 +0100

    improve docs

--- runtime/ajuna/src/benchmark_helpers.rs
+++ runtime/ajuna/src/benchmark_helpers.rs
@@ -1,10 +1,25 @@
+// Ajuna Node
+// Copyright (C) 2022 BlogaTech AG
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 pub mod asset_rate {
 	use frame_support::traits::fungible::NativeOrWithId;
 	use pallet_asset_rate::AssetKindFactory;
 	use parachains_common::AssetIdForTrustBackedAssets;
 
-	/// Provides a factory method for the [`VersionedLocatableAsset`].
-	/// The location of the asset is determined as a Parachain with an ID equal to the passed seed.
+	/// Provides a factory method for the [`NativeOrWithId<AssetIdForTrustBackedAssets>`].
 	pub struct AssetRateArguments;
 
 	pub type AssetKind = NativeOrWithId<AssetIdForTrustBackedAssets>;
@@ -26,9 +41,11 @@ pub mod treasury {
 
 	pub type AssetKind = NativeOrWithId<AssetIdForTrustBackedAssets>;
 
-	/// Provide factory methods for the [`VersionedLocatableAsset`] and the `Beneficiary` of the
-	/// [`VersionedLocation`]. The location of the asset is determined as a Parachain with an
-	/// ID equal to the passed seed.
+	/// Provide factory methods for the [`NativeOrWithId<AssetIdForTrustBackedAssets>`] and
+	/// the `Beneficiary` for [`AccountId`].
+	///
+	/// This is slightly different from Polkadot's implementation, which uses a
+	/// [`VersionedLocatableAsset`] for the AssetId, as the asset lives on another chain.
 	pub struct TreasuryArguments<Parents = ConstU8<0>, ParaId = ConstU32<0>>(
 		PhantomData<(Parents, ParaId)>,
 	);
